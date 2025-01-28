1c4ab40690dd6b4e798d452f42d80a29228f66a4
CCBaiden <ceren.baiden@gmail.com>
2025-01-28 19:47:19 +0300

    revised package.json and vite files

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-js-hw-09/', // **Buraya projenizin adını yazın**
     build: {
       sourcemap: true,
       rollupOptions: {
